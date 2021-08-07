e574ff3a40a568b8582abf4388ec8c7291a36120
DongWon Sehr <ddoro36@gmail.com>
2021-08-08 07:50:56 +0900

    fix syntax error

--- js/todo.js
+++ js/todo.js
@@ -3,7 +3,7 @@ const dayList = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 function runClock() {
 	const today = new Date();
-	clock.innerText = `${today.getFullYear}-${today.getMonth + 1}-${today.getDate} ${dayList[today.getDay]} ${String(today.getHours).padStart(2, "0")}:${String(today.getMinutes).padStart(2, "0")}:${String(today.getSeconds).padStart(2, "0")}`
+	clock.innerText = `${today.getFullYear}-${today.getMonth + 1}-${today.getDate} ${dayList[today.getDay]} ${String(today.getHours).padStart(2, "0")}:${String(today.getMinutes).padStart(2, "0")}:${String(today.getSeconds).padStart(2, "0")}`;
 }
 
 runClock();
