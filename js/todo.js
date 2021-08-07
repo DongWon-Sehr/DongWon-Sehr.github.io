96151f4425f8cac61bc77727f3420d47eddae372
DongWon Sehr <ddoro36@gmail.com>
2021-08-08 07:48:31 +0900

    + clock

---
+++ js/todo.js
@@ -0,0 +1,10 @@
+const clock = document.querySelector("#clock");
+const dayList = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+function runClock() {
+	const today = new Date();
+	clock.innerText = `${today.getFullYear}-${today.getMonth + 1}-${today.getDate} ${dayList[today.getDay]} ${String(today.getHours).padStart(2, "0")}:${String(today.getMinutes).padStart(2, "0")}:${String(today.getSeconds).padStart(2, "0")}`
+}
+
+runClock();
+setInterval(runClock, 1000);
