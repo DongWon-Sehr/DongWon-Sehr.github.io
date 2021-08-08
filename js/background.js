19382bb366701c5fbc52c197df7ed08b154e0a19
DongWon Sehr <dongwon@its-newid.com>
2021-08-09 02:03:23 +0900

    + bg image

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const imgFileNames = [
+	"landscape1.jpg"
+	, "landscape2.jpg"
+	, "landscape3.jpg"
+	, "landscape4.jpg"
+];
+
+const selectedPath = imgFileNames[Math.floor(Math.random() * imgFileNames.length)];
+const imgUrl = `img/${selectedPath}`;
+
+document.body.style.backgroundImage = `url("${imgUrl}")`;
