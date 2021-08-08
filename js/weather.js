1027ece8a89a574578dd42353a391019c43435a0
DongWon Sehr <dongwon@its-newid.com>
2021-08-09 01:43:06 +0900

    fix api url

--- js/weather.js
+++ js/weather.js
@@ -8,7 +8,7 @@ const API_KEY = "899afd58c137de6ebb86f820e2352a24";
 function onGeoSuccess(position) {
 	const lat = position.coords.latitude;
 	const lon = position.coords.longitude;
-	const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 	fetch(url).then(response => response.json()).then(data => {
 		const city = document.querySelector("#city");
 		city.innerText = `Hi ${data.name}!`;
